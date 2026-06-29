2da4919e062a8c0230b733d674bf2e85d0d7ec66
kmilomonroy14 <camiloernestormm@gmail.com>
2026-06-29 00:38:12 -0500

    add user class whit basic validations and schema

---
+++ schemas/user.js
@@ -0,0 +1,10 @@
+import db from '../config/config.js';
+
+await db.exec(/*sql*/`
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  );
+`);
