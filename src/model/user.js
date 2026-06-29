3a3ff2f9e0f3d6d64a995d7c02c1fd27508458c4
kmilomonroy14 <camiloernestormm@gmail.com>
2026-06-29 00:40:53 -0500

    add user class whit basic validations and schema

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,13 @@
-class User {
-    constructor(id=null, name, email, password) {
 
+
+export class User {
+    constructor(id=null, name, email, password) {
+        if (name === undefined || email === undefined || password === undefined) {
+            throw new Error("Name, email, o password son requeridos");
+        }
+        if (!email.includes("@")) {
+            throw new Error("Email inválido");
+        }
         this.id = id;
         this.name = name;
         this.email = email;
